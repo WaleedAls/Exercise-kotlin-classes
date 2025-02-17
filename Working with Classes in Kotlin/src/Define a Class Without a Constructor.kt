530736f9755bc3af57ef591ff78de73bdcd4a148
WaleedAls <swyd16@gmail.com>
2025-02-17 15:10:11 +0300

    Classes excersice

---
+++ Working with Classes in Kotlin/src/Define a Class Without a Constructor.kt
@@ -0,0 +1,16 @@
+fun main () {
+    val myBook = Book1()
+    myBook.title = "Hello World"
+    myBook.author = "Michael Jackson"
+    myBook.publisher = "Michael Jackson"
+}
+
+
+class Book1 {
+
+    var title: String = ""
+    var author: String = ""
+    var publisher: String = ""
+}
+
+
